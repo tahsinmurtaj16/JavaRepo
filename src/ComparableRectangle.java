1d14498de418d5e63bdde1ac981ce354b5d65e13
tahsinmurtaj16 <sheikhmurtaj1@gmail.com>
2019-11-24 17:37:06 +0800

    sixth commit

---
+++ src/ComparableRectangle.java
@@ -0,0 +1,24 @@
+//package java.lang.Comparable;
+ //interface Comparable<ComparableRectangle>{
+   // public int compareTo(ComparableRectangle o);
+//}
+public class ComparableRectangle extends RectangleFromSimpleGeometricObject
+        implements Comparable<ComparableRectangle> {
+    /** Construct a ComparableRectangle with specified properties */
+    public ComparableRectangle(double width, double height) {
+        super(width, height);
+    }
+    @Override // Implement the compareTo method defined in Comparable
+    public int compareTo(ComparableRectangle o) {
+        if (getArea() > o.getArea())
+            return 1;
+        else if (getArea() < o.getArea())
+            return -1;
+        else
+            return 0;
+    }
+    @Override // Implement the toString method in GeometricObject
+    public String toString() {
+        return super.toString() + " Area: " + getArea();
+    }
+}
