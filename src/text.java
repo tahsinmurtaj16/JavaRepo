28c48be8a688e94aa9c66a699de507f8ad0da999
tahsinMurtaj16 <sheikhmurtaj1@gmail.com>
2019-11-24 18:34:09 +0800

    Update text.java

--- src/text.java
+++ src/text.java
@@ -1,5 +1,32 @@
 public class text {
     public static void main(String[] args) {
-        System.out.println("Test");
+        Person[] persons = {new Person(3), new Person(4), new Person(1),new Person(2)};
+        java.util.Arrays.sort(persons);
+        for (Person p:persons
+             ) {
+            System.out.print(p+" ");
+        }
+    }
+}
+
+class Person implements Comparable<Person>{
+    private int id;
+
+    Person(int id) {
+        this.id = id;
+    }
+    @Override
+    public int compareTo(Person o)
+    {
+        if(id > o.id)
+            return 1;
+        else if (id < o.id)
+            return -1;
+        else
+            return 0;
+    }
+    public String toString()
+    {
+        return "ID: "+this.id;
     }
 }
